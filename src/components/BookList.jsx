99cd57e878a4b3a7b936db82ba518c08aedc4309
Qui Nguyen <qui@gmail.com>
2024-03-06 22:33:40 -0500

    first commit

---
+++ src/components/BookList.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+import BookDetails from "./BookDetails";
+
+const BookList = () => {
+  const { books } = useContext(BookContext);
+  return books.length ? (
+    <div className="book-list">
+      <ul>
+        {books.map((book) => (
+          <BookDetails book={book} key={book.id} />
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty">No books to read.</div>
+  );
+};
+
+export default BookList;
