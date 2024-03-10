6a29221caaa223e1286c5ad35186bd40e2be59ed
Qui Nguyen <qui@gmail.com>
2024-03-09 21:43:12 -0500

    first commit

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,10 +1,16 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import bookReducer from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
